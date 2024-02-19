3b22392e6ccd6eac45cccb0c42256b7e0d027c54
otaku.prof <norman_lee@sutd.edu.sg>
2024-02-19 09:58:13 +0800

    first commit - edited Singleton

--- JavaExamples/src/main/java/Example56/Connection.java
+++ JavaExamples/src/main/java/Example56/Connection.java
@@ -2,17 +2,11 @@ package Example56;
 
 public class Connection {
 
-    private static Connection connection;
 
-    private Connection(){
+
+    Connection(){
     }
 
-    public static Connection getInstance(){
-        if( connection == null){
-            connection = new Connection();
-        }
-        return connection;
-    }
 
     public String getData(){
         return "here's some data";
